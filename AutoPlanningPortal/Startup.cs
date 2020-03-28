42329e8b24d58a777bba92c7611e57f4b9cbc15d
Venus_Hu <tung19940530@gmail.com>
2020-03-28 15:20:07 +0800

    init

---
+++ AutoPlanningPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AutoPlanningPortal.Startup))]
+namespace AutoPlanningPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
